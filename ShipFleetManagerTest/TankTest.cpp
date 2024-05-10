5433df5ad31d583c20b81a24a77b228ddf88b558
eryk-urbanski <eryk.urbanski14@gmail.com>
2024-05-10 14:07:32 +0200

    Add setCurrentVolume method unit tests

--- ShipFleetManagerTest/TankTest.cpp
+++ ShipFleetManagerTest/TankTest.cpp
@@ -14,3 +14,22 @@ TEST(TankTest, ConstructorInvalidCapacity) {
     EXPECT_THROW(Tank(-1.0, DIESEL), std::invalid_argument);
     EXPECT_THROW(Tank(0.0, DIESEL), std::invalid_argument);
 }
+
+// Test Case 3: Valid setCurrentVolume
+TEST(TankTest, ValidSetCurrentVolume) {
+    Tank tank(500.0, DIESEL);
+    EXPECT_NO_THROW(tank.setCurrentVolume(50.0));
+    EXPECT_DOUBLE_EQ(tank.getCurrentVolume(), 50.0);
+}
+
+// Test Case 4: Negative Volume
+TEST(TankTest, NegativeVolume) {
+    Tank tank(500.0, DIESEL);
+    EXPECT_THROW(tank.setCurrentVolume(-1.0), std::invalid_argument);
+}
+
+// Test Case 5: Exceed Capacity
+TEST(TankTest, ExceedCapacity) {
+    Tank tank(500.0, DIESEL);
+    EXPECT_THROW(tank.setCurrentVolume(1500.0), std::invalid_argument);
+}
